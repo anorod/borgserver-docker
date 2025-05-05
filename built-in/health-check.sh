c26c705a4e291ddc3a599b46f029deffdeb8cfb1
anorod <anorod@gmail.com>
2025-05-05 14:09:47 +0200

    Now we will kill SSHD so there are no attemp of backups when the filesystem is wrong. Once the filesystem is ok it will reboot the container. Finally added the logging to docker output.

--- built-in/health-check.sh
+++ built-in/health-check.sh
@@ -2,20 +2,23 @@
 
 # Check if the environment variable is set
 if [ -z "$HEALTHCHECK_PATH" ]; then
-  echo "HEALTHCHECK_PATH environment variable not set. Not checking filesystem"
+  echo `date` ":HEALTHCHECK_PATH environment variable not set. Not checking filesystem" >> /proc/1/fd/1
 else 
   # Check if the file exists
   if [ ! -f "$HEALTHCHECK_PATH" ]; then
-    echo "File not found at $HEALTHCHECK_PATH" >&2
+    echo `date` "File not found at $HEALTHCHECK_PATH" >> /proc/1/fd/1
+    echo `date` "Kill SSH Daemon to prevent wrong service." >> /proc/1/fd/1
+    kill -15 $(pidof sshd)
     exit 1
   fi
 fi
 
 # Check if ssh is running (basic check)
 if netstat -an | grep :22 > /dev/null; then
-  echo "SSH service is running."
+  #echo `date` "Healthcheck OK. SSH service is running." >> /proc/1/fd/1
 else
-  echo "SSH service is not running." >&2
+  echo `date` "SSH service is not running, restarting container." >> /proc/1/fd/1
+  reboot
   exit 1
 fi
 
